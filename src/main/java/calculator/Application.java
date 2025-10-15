884ca3021c89cd35d08a72cce3ca8efd14a10230
YUNJAEGOONER <coygyj@gmail.com>
2025-10-15 20:08:08 +0900

    feat: 커스텀 구분자를 반환하는 메서드 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,6 +8,16 @@ public class Application {
         return Console.readLine();
     }
 
+    public static Character getCustomDelimiter(String input){
+        if(input.length() >= 4){
+            String customSequence = input.substring(0, 5);
+            if(customSequence.matches("//.\\\\n")){
+                return customSequence.charAt(2);
+            }
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String input = getInput();
