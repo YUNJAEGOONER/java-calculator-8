c38eb131098dfad7959419caaeabf805ec95bf1b
YUNJAEGOONER <coygyj@gmail.com>
2025-10-17 01:29:44 +0900

    feat: 시작 문자와 끝 문자에 대한 예외처리 수행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,13 +9,7 @@ public class Application {
     }
 
     public static Character getCustomDelimiter(String input){
-        if(input.length() >= 4){
-            String customSequence = input.substring(0, 5);
-            if(customSequence.matches("//.\\\\n")){ //맨앞이 //구분자\n 인 경우
-               return customSequence.charAt(2);
-            }
-        }
-        return null;
+        return input.charAt(3);
     }
 
     public static String convertCustomToNormalDelimiter(String input, char cDelimiter){
@@ -34,9 +28,13 @@ public class Application {
         }
 
         String [] operandArr = input.split(",|:"); // 기본 구분자 (쉼표 또는 콜론);
+
         for(int i = 0 ; i < operandArr.length ; i ++) {
             if(operandArr[i] == null){
                 throw new IllegalArgumentException("유효하지 않은 입력입니다.(예 - 구분자가 연속되서 등장하면 안됩니다.)");
+            }
+            if(operandArr[i].length() >= 2 && operandArr[i].startsWith("0")){
+                throw new IllegalArgumentException("유효하지 않은 입력입니다.(0으로 시작되는 숫자는 0 뿐입니다.)");
             }
         }
         return operandArr;
@@ -50,13 +48,39 @@ public class Application {
         return sum;
     }
 
+    public static boolean isNumber(char num){
+        return ('0' <= num && num <= '9');
+    }
+
+    public static boolean startWithCustomDelimiter(String input){
+        if(input.length() >= 4){
+            String customSequence = input.substring(0, 5);
+            return customSequence.matches("//.\\\\n"); // 맨앞이 //구분자\n 인 경우
+        }
+        return false;
+    }
+
+    //맨 앞이 숫자로 시작하거나 custom 문자를 위한
+    public static void checkAvailableInput(String input){
+        if(input.isBlank()){
+            throw new IllegalArgumentException("수식을 입력하지 않았습니다.");
+        }
+        if(!(isNumber(input.charAt(0)) || startWithCustomDelimiter(input))) { //시작을 검사
+            throw new IllegalArgumentException("수식은 양의 정수 또는 //(커스텀 구분자)\\n로 시작되어야 합니다");
+        }
+        if(!isNumber(input.charAt(input.length() - 1))){ //끝을 검사
+            throw new IllegalArgumentException("수식은 양의 정수로 끝나야 합니다.");
+        }
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
         String input = getInput();
 
-        Character customDelimiter = getCustomDelimiter(input);
+        checkAvailableInput(input);
 
-        if(customDelimiter != null){
+        if(startWithCustomDelimiter(input)){
+            Character customDelimiter = getCustomDelimiter(input);
             input = convertCustomToNormalDelimiter(input, customDelimiter);
         }
 
--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -21,6 +22,22 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("-1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_구분자가_연이어_등장하는_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,,,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 수식에_문자가_등장하는_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,,,woowa:course,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
